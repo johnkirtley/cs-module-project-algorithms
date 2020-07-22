afecd3b40204ab3e7276789638e82b158ec637db
johnkirtley <kirtleyj16@gmail.com>
2020-07-22 11:06:52 -0700

    :zap: Completed Single_Number Challenge

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,22 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    count = {}
+
+    for i in range(len(arr)):
+        current = arr[i]
+
+        if current not in count:
+            count[current] = 1
+        else:
+            count[current] += 1
+
+    for key, value in count.items():
+        if value == 1:
+            return key
 
 
 if __name__ == '__main__':
